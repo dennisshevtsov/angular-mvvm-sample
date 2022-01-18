62d727b75198df73d5673d4e878477e5bb96a9a4
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-01-18 10:57:32 +0300

    add search todo lists comp

---
+++ src/app/todo-list/components/search-todo-lists/search-todo-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-todo-lists',
+  templateUrl: './search-todo-lists.component.html',
+  styleUrls: ['./search-todo-lists.component.scss']
+})
+export class SearchTodoListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/todo-list/todo-list.module.ts
+++ src/app/todo-list/todo-list.module.ts
@@ -1,10 +1,12 @@
 import { NgModule,     } from '@angular/core';
 import { CommonModule, } from '@angular/common';
 import { AddTodoListComponent } from './components/add-todo-list/add-todo-list.component';
+import { SearchTodoListsComponent } from './components/search-todo-lists/search-todo-lists.component';
 
 @NgModule({
   declarations: [
-    AddTodoListComponent
+    AddTodoListComponent,
+    SearchTodoListsComponent
   ],
   imports: [
     CommonModule,
