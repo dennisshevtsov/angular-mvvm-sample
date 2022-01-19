61ff3f240b2abbee9c185875c288910b9afad125
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-01-20 00:44:46 +0300

    add add todo list task comp

---
+++ src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo-list-task',
+  templateUrl: './add-todo-list-task.component.html',
+  styleUrls: ['./add-todo-list-task.component.scss']
+})
+export class AddTodoListTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/todo-list-task/todo-list-task.module.ts
+++ src/app/todo-list-task/todo-list-task.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddTodoListTaskComponent } from './components/add-todo-list-task/add-todo-list-task.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddTodoListTaskComponent
+  ],
   imports: [
     CommonModule
   ]
