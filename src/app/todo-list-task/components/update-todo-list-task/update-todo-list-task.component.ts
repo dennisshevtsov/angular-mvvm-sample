bcf79a983a5c335bb3b73f397d4925c989af46ae
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-01-20 00:45:56 +0300

    add update todo list task comp

---
+++ src/app/todo-list-task/components/update-todo-list-task/update-todo-list-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-todo-list-task',
+  templateUrl: './update-todo-list-task.component.html',
+  styleUrls: ['./update-todo-list-task.component.scss']
+})
+export class UpdateTodoListTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/todo-list-task/todo-list-task.module.ts
+++ src/app/todo-list-task/todo-list-task.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddTodoListTaskComponent } from './components/add-todo-list-task/add-todo-list-task.component';
 import { SearchTodoListTasksComponent } from './components/search-todo-list-tasks/search-todo-list-tasks.component';
+import { UpdateTodoListTaskComponent } from './components/update-todo-list-task/update-todo-list-task.component';
 
 
 
 @NgModule({
   declarations: [
     AddTodoListTaskComponent,
-    SearchTodoListTasksComponent
+    SearchTodoListTasksComponent,
+    UpdateTodoListTaskComponent
   ],
   imports: [
     CommonModule
