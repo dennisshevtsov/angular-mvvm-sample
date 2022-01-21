1be0b2a7413f548232c0a2c3017d4e32c0264900
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-01-21 10:27:05 +0300

    add imports to todo list task

---
+++ src/app/todo-list-task/components/index.ts
@@ -0,0 +1,3 @@
+export * from './add-todo-list-task/add-todo-list-task.component';
+export * from './search-todo-list-tasks/search-todo-list-tasks.component';
+export * from './update-todo-list-task/update-todo-list-task.component';
