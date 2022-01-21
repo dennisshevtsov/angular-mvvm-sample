fbb649bc3a0e75232e24a3bb7fb566088f7d14d3
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-01-21 10:25:35 +0300

    refactor add todo list task comp

--- src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.component.ts
+++ src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
 
 @Component({
-  selector: 'app-add-todo-list-task',
   templateUrl: './add-todo-list-task.component.html',
-  styleUrls: ['./add-todo-list-task.component.scss']
+  styleUrls: [
+    './add-todo-list-task.component.scss',
+  ],
 })
-export class AddTodoListTaskComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class AddTodoListTaskComponent { }
