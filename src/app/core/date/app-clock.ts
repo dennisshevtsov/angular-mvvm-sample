f9ad1902399707a8f9889e5f754e71ff526c0658
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-01-28 15:13:13 +0300

    add app clock

---
+++ src/app/core/date/app-clock.ts
@@ -0,0 +1,26 @@
+import { Injectable, } from '@angular/core';
+
+export const MILLISECONDS_IN_MENUTE = 1 * 60 * 1000;
+export const MILLISECONDS_IN_HOUR   = 1 * 60 * MILLISECONDS_IN_MENUTE;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppClock {
+  public time(date: Date = new Date()): number {
+    return date.getUTCHours() * MILLISECONDS_IN_HOUR +
+           date.getUTCMinutes() * MILLISECONDS_IN_MENUTE;
+  }
+
+  public date(date: Date = new Date()): number {
+    return Date.UTC(date.getUTCFullYear(),
+                    date.getUTCMonth(),
+                    date.getUTCDate());
+  }
+
+  public now(): number {
+    const now: Date = new Date();
+
+    return this.date(now) + this.time(now)
+  }
+}
