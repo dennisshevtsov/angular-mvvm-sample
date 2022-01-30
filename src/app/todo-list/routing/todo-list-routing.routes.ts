119f0ff5c163f28c2cfd53802c7976cce35ad740
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-01-30 11:20:52 +0300

    add todo list routing routes prov

---
+++ src/app/todo-list/routing/todo-list-routing.routes.ts
@@ -0,0 +1,29 @@
+import { Injectable, } from '@angular/core';
+
+import { TodoListRoutingLinks,         } from './todo-list-routing.links';
+import { TODO_LIST_ROUTE_ID_PARAMETER, } from './todo-list-routing.values';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoListRoutingRoutes {
+  public constructor(
+    private readonly links: TodoListRoutingLinks,
+  ) {}
+
+  public addTodoListRoute(): string {
+    return this.convertToRoute(this.links.addTodoListLink());
+  }
+
+  public updateTodoListRoute(): string {
+    return this.convertToRoute(this.links.updateTodoListLink(`:${TODO_LIST_ROUTE_ID_PARAMETER}`));
+  }
+
+  public searchTodoListsRoute(): string {
+    return this.convertToRoute(this.links.searchTodoListsLink());
+  }
+
+  private convertToRoute(link: Array<any>): string {
+    return link.slice(1).join('/');
+  }
+}
