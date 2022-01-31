edd1f47e6134237af32a0103a7c72ef99475cf54
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-01-31 10:59:25 +0300

    add todo list routing routes

--- src/app/todo-list/todo-list-routing.module.ts
+++ src/app/todo-list/todo-list-routing.module.ts
@@ -1,11 +1,39 @@
-import { NgModule,             } from '@angular/core';
+import { Injector, NgModule,   } from '@angular/core';
 import { RouterModule, Routes, } from '@angular/router';
 
-import { SearchTodoListsComponent, } from './components';
+import { AddTodoListComponent,
+         SearchTodoListsComponent,
+         UpdateTodoListComponent,  } from './components';
+import { TodoListRoutingLinks,
+         TodoListRoutingRoutes,    } from './routing';
+
+const options = {
+  providers: [
+    {
+      provide: TodoListRoutingRoutes,
+      deps: [ TodoListRoutingLinks ],
+    },
+    {
+      provide: TodoListRoutingLinks,
+      deps: [],
+    },
+  ],
+}
+const factory = Injector.create(options);
+
+const routeProvider = factory.get(TodoListRoutingRoutes);
 
 const routes: Routes = [
   {
-    path: 'todo',
+    path: routeProvider.addTodoListRoute(),
+    component: AddTodoListComponent,
+  },
+  {
+    path: routeProvider.updateTodoListRoute(),
+    component: UpdateTodoListComponent,
+  },
+  {
+    path: routeProvider.searchTodoListsRoute(),
     component: SearchTodoListsComponent,
   },
 ];
