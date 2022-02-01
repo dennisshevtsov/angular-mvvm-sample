d96afd6fa19edb6ddf158b2236c58aaefdca44c8
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-01 10:48:39 +0300

    add todo list task routing fragments

---
+++ src/app/todo-list-task/routing/todo-list-task-routing.fragments.ts
@@ -0,0 +1,3 @@
+export const TODO_LIST_TASK_ROUTE_ID_PARAMETER  = 'todoListTaskId';
+export const TODO_LIST_TASK_ROUTE_ROOT_FRAGMENT = 'task';
+export const TODO_LIST_TASK_ROUTE_NEW_FRAGMENT  = 'new';
