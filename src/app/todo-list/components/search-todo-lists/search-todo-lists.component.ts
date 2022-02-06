93bc14dfda97249b2931b92ea82ee2fc55d0d1d2
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-06 10:34:43 +0300

    add vm to search todo lists component

--- src/app/todo-list/components/search-todo-lists/search-todo-lists.component.ts
+++ src/app/todo-list/components/search-todo-lists/search-todo-lists.component.ts
@@ -1,9 +1,15 @@
 import { Component, } from '@angular/core';
 
+import { SearchTodoListsViewModel, } from './search-todo-lists.view-model';
+
 @Component({
   templateUrl: './search-todo-lists.component.html',
   styleUrls: [
     './search-todo-lists.component.scss',
   ],
 })
-export class SearchTodoListsComponent { }
+export class SearchTodoListsComponent {
+  public constructor(
+    public vm: SearchTodoListsViewModel,
+  ) {}
+}
--- src/app/todo-list/components/search-todo-lists/search-todo-lists.view-model.ts
+++ src/app/todo-list/components/search-todo-lists/search-todo-lists.view-model.ts
@@ -1,2 +1,8 @@
+import { Injectable, } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
 export class SearchTodoListsViewModel {
+  public search(): void {}
 }
