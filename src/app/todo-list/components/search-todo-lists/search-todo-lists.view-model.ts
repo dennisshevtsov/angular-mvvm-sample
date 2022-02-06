7a05bba23ea8c6e108e281dfcfc57731a441b181
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-06 10:45:11 +0300

    add datasource in search todo lists vm

--- src/app/todo-list/components/search-todo-lists/search-todo-lists.view-model.ts
+++ src/app/todo-list/components/search-todo-lists/search-todo-lists.view-model.ts
@@ -1,8 +1,12 @@
 import { Injectable, } from '@angular/core';
 
+import { SearchTodoListsRecordResponseDto, } from '../../../todo-list-api';
+
 @Injectable({
   providedIn: 'root',
 })
 export class SearchTodoListsViewModel {
+  public datasource: SearchTodoListsRecordResponseDto[] | undefined;
+
   public search(): void {}
 }
