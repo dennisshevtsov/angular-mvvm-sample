18b82541cca3efe656f20dcdd3c9969ecbd65377
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-06 10:41:20 +0300

    use vm in search todo lists component

--- src/app/todo-list/components/search-todo-lists/search-todo-lists.component.ts
+++ src/app/todo-list/components/search-todo-lists/search-todo-lists.component.ts
@@ -1,4 +1,4 @@
-import { Component, } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 
 import { SearchTodoListsViewModel, } from './search-todo-lists.view-model';
 
@@ -8,8 +8,24 @@ import { SearchTodoListsViewModel, } from './search-todo-lists.view-model';
     './search-todo-lists.component.scss',
   ],
 })
-export class SearchTodoListsComponent {
+export class SearchTodoListsComponent implements OnInit {
   public constructor(
     public vm: SearchTodoListsViewModel,
   ) {}
+
+  public ngOnInit(): void {
+      this.vm.search();
+  }
+
+  public get addTodoListLink(): string {
+    return '';
+  }
+
+  public updateTodoListLink(todoListId: string | number) : string {
+    return '';
+  }
+
+  public searchTodoListTasksLink(todoListId: string | number) : string {
+    return '';
+  }
 }
