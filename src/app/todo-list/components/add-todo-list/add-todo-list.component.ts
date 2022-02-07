3dac5afde40395bb25ce87331b3f6b9ad31ed86a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-07 10:43:39 +0300

    use vm in add todo list comp

--- src/app/todo-list/components/add-todo-list/add-todo-list.component.ts
+++ src/app/todo-list/components/add-todo-list/add-todo-list.component.ts
@@ -1,9 +1,15 @@
 import { Component, } from '@angular/core';
 
+import { AddTodoListViewModel, } from './add-todo-list.view-model';
+
 @Component({
   templateUrl: './add-todo-list.component.html',
   styleUrls: [
     './add-todo-list.component.scss',
   ],
 })
-export class AddTodoListComponent { }
+export class AddTodoListComponent {
+  public constructor(
+    public readonly vm: AddTodoListViewModel,
+  ) {}
+}
