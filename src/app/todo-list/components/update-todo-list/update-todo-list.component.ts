f6b25234d543ca0e5b9e89f94be5d7c2d0a58e83
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-07 10:44:23 +0300

    use vm in update todo list comp

--- src/app/todo-list/components/update-todo-list/update-todo-list.component.ts
+++ src/app/todo-list/components/update-todo-list/update-todo-list.component.ts
@@ -1,9 +1,15 @@
 import { Component, } from '@angular/core';
 
+import { UpdateTodoListViewModel, } from './update-todo-list.view-model';
+
 @Component({
   templateUrl: './update-todo-list.component.html',
   styleUrls: [
     './update-todo-list.component.scss',
   ],
 })
-export class UpdateTodoListComponent { }
+export class UpdateTodoListComponent {
+  public constructor(
+    public readonly vm: UpdateTodoListViewModel,
+  ) {}
+}
--- src/app/todo-list/components/update-todo-list/update-todo-list.view-model.ts
+++ src/app/todo-list/components/update-todo-list/update-todo-list.view-model.ts
@@ -1,2 +1,3 @@
 export class UpdateTodoListViewModel {
+  public update(): void {}
 }
