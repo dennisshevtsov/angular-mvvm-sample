2f1d4d43e22f524594a3dd4f43c9ef0376c86347
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-13 10:42:41 +0300

    use form base in add todo list task comp

--- src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.component.ts
+++ src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { FormComponentBase,        } from 'src/app/core';
 import { AddTodoListTaskViewModel, } from './add-todo-list-task.view-model';
 
 @Component({
@@ -9,18 +10,24 @@ import { AddTodoListTaskViewModel, } from './add-todo-list-task.view-model';
     './add-todo-list-task.component.scss',
   ],
 })
-export class AddTodoListTaskComponent {
+export class AddTodoListTaskComponent extends FormComponentBase {
   public constructor(
     public readonly vm: AddTodoListTaskViewModel,
 
     private readonly fb: FormBuilder,
-  ) {}
+  ) {
+    super();
+  }
 
-  public get form(): FormGroup {
-    return this.fb.group({});
+  public get backLink(): string {
+    return '';
   }
 
   public onOkPressed(): void {
     this.vm.add();
+  }
+
+  protected buildForm(): FormGroup {
+    return this.fb.group({});
   }
 }
