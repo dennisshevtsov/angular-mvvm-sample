1c474c041e3e8b1cc912112595e74010873c61a1
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-14 14:52:31 +0300

    add tasks prop in search todo list tasks comp

--- src/app/todo-list-task/components/search-todo-list-tasks/search-todo-list-tasks.view-model.ts
+++ src/app/todo-list-task/components/search-todo-list-tasks/search-todo-list-tasks.view-model.ts
@@ -1,3 +1,9 @@
+import { SearchTodoListsRecordResponseDto } from 'src/app/todo-list-api';
+
 export class SearchTodoListTasksViewModel {
+  public get tasks(): SearchTodoListsRecordResponseDto[] {
+    return [];
+  }
+
   public search(): void {}
 }
