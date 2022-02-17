994c1df111ebaa6bf2d79c394eecc162d19709de
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-17 10:53:03 +0300

    fix update todo list task comp

--- src/app/todo-list-task/components/update-todo-list-task/update-todo-list-task.component.ts
+++ src/app/todo-list-task/components/update-todo-list-task/update-todo-list-task.component.ts
@@ -1,4 +1,7 @@
-import { Component, } from '@angular/core';
+import { Component,              } from '@angular/core';
+import { FormBuilder, FormGroup, } from '@angular/forms';
+
+import { FormComponentBase, } from 'src/app/core';
 
 @Component({
   templateUrl: './update-todo-list-task.component.html',
@@ -6,4 +9,20 @@ import { Component, } from '@angular/core';
     './update-todo-list-task.component.scss',
   ],
 })
-export class UpdateTodoListTaskComponent { }
+export class UpdateTodoListTaskComponent extends  FormComponentBase {
+  public constructor(
+    private readonly fb: FormBuilder,
+  ) {
+    super();
+  }
+
+  public get backLink(): string {
+    return '';
+  }
+
+  public onOkPressed(): void {}
+
+  protected buildForm(): FormGroup {
+    return this.fb.group({});
+  }
+}
