a2b3821abd9fb0993c282e3b0c99a5da259db083
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-18 15:26:03 +0300

    fix add todo list comp

--- src/app/todo-list/components/add-todo-list/add-todo-list.component.ts
+++ src/app/todo-list/components/add-todo-list/add-todo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormComponentBase } from 'src/app/core';
 
 import { AddTodoListViewModel, } from './add-todo-list.view-model';
 
@@ -8,10 +10,13 @@ import { AddTodoListViewModel, } from './add-todo-list.view-model';
     './add-todo-list.component.scss',
   ],
 })
-export class AddTodoListComponent {
+export class AddTodoListComponent extends FormComponentBase {
   public constructor(
     public readonly vm: AddTodoListViewModel,
-  ) {}
+    private readonly fb: FormBuilder,
+  ) {
+    super();
+  }
 
   public get backLink(): string {
     return '';
@@ -19,5 +24,9 @@ export class AddTodoListComponent {
 
   public onOkPressed(): void {
     this.vm.add();
+  }
+
+  protected buildForm(): FormGroup {
+      return this.fb.group({});
   }
 }
