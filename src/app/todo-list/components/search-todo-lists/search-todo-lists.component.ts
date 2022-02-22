379495bf6e70692e60d71838e094a4de79336886
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-22 11:21:31 +0300

    use link prov in search todo lists comp

--- src/app/todo-list/components/search-todo-lists/search-todo-lists.component.ts
+++ src/app/todo-list/components/search-todo-lists/search-todo-lists.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, } from '@angular/core';
 
 import { ModalComponent,                   } from 'src/app/core';
 import { SearchTodoListsRecordResponseDto, } from 'src/app/todo-list-api';
+import { TodoListTaskLinkProvider,         } from 'src/app/todo-list-task/routing';
+import { TodoListLinkProvider,             } from 'src/app/todo-list/routing';
 import { SearchTodoListsViewModel,         } from './search-todo-lists.view-model';
 
 @Component({
@@ -16,22 +18,29 @@ export class SearchTodoListsComponent implements OnInit {
 
   public constructor(
     public vm: SearchTodoListsViewModel,
+
+    private readonly todoListLinks: TodoListLinkProvider,
+    private readonly todoListTaskLinks: TodoListTaskLinkProvider,
   ) {}
 
   public ngOnInit(): void {
       this.vm.search();
   }
 
-  public get addTodoListLink(): string {
-    return '';
+  public get addTodoListLink(): any[] {
+    return this.todoListLinks.addTodoListLink();
   }
 
-  public updateTodoListLink(record: SearchTodoListsRecordResponseDto) : string {
-    return '';
+  public updateTodoListLink(
+    record: SearchTodoListsRecordResponseDto)
+    : any[] {
+    return this.todoListLinks.updateTodoListLink(record.todoListId);
   }
 
-  public searchTodoListTasksLink(record: SearchTodoListsRecordResponseDto) : string {
-    return '';
+  public searchTodoListTasksLink(
+    record: SearchTodoListsRecordResponseDto)
+    : any[] {
+    return this.todoListTaskLinks.searchTodoListTasksLink(record.todoListId);
   }
 
   public onDeletePressed(record: SearchTodoListsRecordResponseDto): void {
