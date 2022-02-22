40a8fce0c174e43811ba59a8ef01b875f5391928
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-22 11:21:48 +0300

    use link prov in update todo list comp

--- src/app/todo-list/components/update-todo-list/update-todo-list.component.ts
+++ src/app/todo-list/components/update-todo-list/update-todo-list.component.ts
@@ -2,6 +2,7 @@ import { Component,             } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { FormComponentBase,       } from 'src/app/core';
+import { TodoListLinkProvider,    } from 'src/app/todo-list/routing';
 import { UpdateTodoListViewModel, } from './update-todo-list.view-model';
 
 @Component({
@@ -13,13 +14,15 @@ import { UpdateTodoListViewModel, } from './update-todo-list.view-model';
 export class UpdateTodoListComponent extends FormComponentBase {
   public constructor(
     public readonly vm: UpdateTodoListViewModel,
+
     private readonly fb: FormBuilder,
+    private readonly todoListLinks: TodoListLinkProvider,
   ) {
     super();
   }
 
-  public get backLink(): string {
-    return '';
+  public get backLink(): any[] {
+    return this.todoListLinks.searchTodoListsLink();
   }
 
   public onOkPressed(): void {
