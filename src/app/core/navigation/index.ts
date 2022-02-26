730dae215493ff75760342de562093eb49c66c82
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-26 11:03:46 +0300

    add core nav

---
+++ src/app/core/navigation/index.ts
@@ -0,0 +1,4 @@
+export * from './todo-list-links';
+export * from './todo-list-routing-fragments';
+export * from './todo-list-task-links';
+export * from './todo-list-task-routing-fragments';
---
+++ src/app/core/navigation/todo-list-routing-fragments.ts
@@ -0,0 +1,3 @@
+export const TODO_LIST_ROUTE_ID_PARAMETER  = 'todoListId';
+export const TODO_LIST_ROUTE_ROOT_FRAGMENT = 'todo';
+export const TODO_LIST_ROUTE_NEW_FRAGMENT  = 'new';
