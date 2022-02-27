0e68eee3488d37b9b246c63e14abd4d5c172967c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-02-27 11:33:07 +0300

    impl routing in todo list task module

--- src/app/todo-list-task/todo-list-task-routing.module.ts
+++ src/app/todo-list-task/todo-list-task-routing.module.ts
@@ -1,11 +1,42 @@
-import { NgModule,     } from '@angular/core';
-import { RouterModule, } from '@angular/router';
+import { NgModule,             } from '@angular/core';
+import { RouterModule, Routes, } from '@angular/router';
 
-import { TODO_LIST_TASK_ROUTES } from './routing';
+import { TodoListTaskLinks,
+         TODO_LIST_ROUTE_ID_PARAMETER,
+         TODO_LIST_TASK_ROUTE_ID_PARAMETER, } from 'src/app/core';
+import { AddTodoListTaskComponent,
+         SearchTodoListTasksComponent,
+         UpdateTodoListTaskComponent,       } from './components';
+
+function convertToRoute(link: Array<any>): string {
+  return link.slice(1).join('/');
+}
+
+const links = new TodoListTaskLinks();
+
+const routes: Routes = [
+  {
+    path: convertToRoute(
+      links.searchTodoListTasksLink(`:${TODO_LIST_ROUTE_ID_PARAMETER}`)),
+    component: SearchTodoListTasksComponent,
+  },
+  {
+    path: convertToRoute(
+      links.addTodoListTaskLink(`:${TODO_LIST_ROUTE_ID_PARAMETER}`)),
+    component: AddTodoListTaskComponent,
+  },
+  {
+    path: convertToRoute(
+      links.updateTodoListTaskLink(
+        `:${TODO_LIST_ROUTE_ID_PARAMETER}`,
+        `:${TODO_LIST_TASK_ROUTE_ID_PARAMETER}`)),
+    component: UpdateTodoListTaskComponent,
+  },
+];
 
 @NgModule({
   imports: [
-    RouterModule.forChild(TODO_LIST_TASK_ROUTES),
+    RouterModule.forChild(routes),
   ],
   exports: [
     RouterModule,
