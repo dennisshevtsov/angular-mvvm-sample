b5c9fae8c4f606091ae7b2caa94301994d0fc862
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-03-01 19:41:10 +0300

    use links in search todo list tasks comp

--- src/app/todo-list-task/components/search-todo-list-tasks/search-todo-list-tasks.component.ts
+++ src/app/todo-list-task/components/search-todo-list-tasks/search-todo-list-tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild, } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
-import { ModalComponent,                       } from 'src/app/core';
+import { ModalComponent,
+         TodoListLinks, TodoListTaskLinks, TODO_LIST_ROUTE_ID_PARAMETER,     } from 'src/app/core';
 import { SearchTodoListTasksRecordResponseDto, } from 'src/app/todo-list-task/api';
 import { SearchTodoListTasksViewModel,         } from './search-todo-list-tasks.view-model';
 
@@ -14,14 +16,26 @@ export class SearchTodoListTasksComponent implements OnInit {
   @ViewChild('modal')
   private modalRef!: ModalComponent;
 
+  private todoListId!: number | string;
   private recordValue: SearchTodoListTasksRecordResponseDto | undefined;
 
   public constructor(
     public readonly vm: SearchTodoListTasksViewModel,
+
+    private readonly route: ActivatedRoute,
+    private readonly todoListLinks: TodoListLinks,
+    private readonly todoListTaskLinks: TodoListTaskLinks,
   ) {}
 
   public ngOnInit(): void {
-    this.vm.search();
+    this.route.paramMap.subscribe(params => {
+      const todoListId = params.get(TODO_LIST_ROUTE_ID_PARAMETER);
+
+      if (todoListId) {
+        this.vm.todoListId = todoListId;
+        this.vm.search();
+      }
+    });
   }
 
   public get selected(): SearchTodoListTasksRecordResponseDto {
@@ -32,17 +46,17 @@ export class SearchTodoListTasksComponent implements OnInit {
     this.recordValue = record;
   }
 
-  public get backLink(): string {
-    return '';
+  public get backLink(): any[] {
+    return this.todoListLinks.searchTodoListsLink();
   }
 
-  public get addTodoListTaskLink(): string {
-    return '';
+  public get addTodoListTaskLink(): any[] {
+    return this.todoListTaskLinks.addTodoListTaskLink(this.vm.todoListId);
   }
 
   public updateTodoListTaskLink(
-    todoListTaskId: number | string) : string {
-    return '';
+    todoListTaskId: number | string) : any[] {
+      return this.todoListTaskLinks.updateTodoListTaskLink(this.vm.todoListId, todoListTaskId);
   }
 
   public onCompletedChanged(
--- src/app/todo-list-task/components/search-todo-list-tasks/search-todo-list-tasks.view-model.ts
+++ src/app/todo-list-task/components/search-todo-list-tasks/search-todo-list-tasks.view-model.ts
@@ -1,6 +1,16 @@
 import { SearchTodoListTasksRecordResponseDto, } from 'src/app/todo-list-task/api';
 
 export class SearchTodoListTasksViewModel {
+  private todoListIdValue: number | string | undefined;
+
+  public get todoListId(): number | string {
+    return this.todoListIdValue ?? 0;
+  }
+
+  public set todoListId(todoListId: number | string) {
+    this.todoListIdValue = todoListId;
+  }
+
   public get tasks(): SearchTodoListTasksRecordResponseDto[] {
     return [];
   }
