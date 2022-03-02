b4b4ebdb55f91b93d0c5fdbd4804e65d9cb0ae73
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-03-02 13:02:07 +0300

    add links to add todo list task comp

--- src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.component.ts
+++ src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.component.ts
@@ -1,8 +1,10 @@
-import { Component, } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit,      } from '@angular/core';
+import { FormBuilder, FormGroup, } from '@angular/forms';
+import { ActivatedRoute,         } from '@angular/router';
 
-import { FormComponentBase,        } from 'src/app/core';
-import { AddTodoListTaskViewModel, } from './add-todo-list-task.view-model';
+import { FormComponentBase, TodoListTaskLinks,
+         TODO_LIST_ROUTE_ID_PARAMETER,         } from 'src/app/core';
+import { AddTodoListTaskViewModel,             } from './add-todo-list-task.view-model';
 
 @Component({
   templateUrl: './add-todo-list-task.component.html',
@@ -10,17 +12,31 @@ import { AddTodoListTaskViewModel, } from './add-todo-list-task.view-model';
     './add-todo-list-task.component.scss',
   ],
 })
-export class AddTodoListTaskComponent extends FormComponentBase {
+export class AddTodoListTaskComponent
+  extends FormComponentBase
+  implements OnInit {
   public constructor(
     public readonly vm: AddTodoListTaskViewModel,
 
+    private readonly route: ActivatedRoute,
     private readonly fb: FormBuilder,
+    private readonly todoListTaskLinks: TodoListTaskLinks,
   ) {
     super();
   }
 
-  public get backLink(): string {
-    return '';
+  public ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const todoListId = params.get(TODO_LIST_ROUTE_ID_PARAMETER);
+
+      if (todoListId) {
+        this.vm.todoListId = todoListId;
+      }
+    });
+  }
+
+  public get backLink(): any[] {
+    return this.todoListTaskLinks.searchTodoListTasksLink(this.vm.todoListId);
   }
 
   public onOkPressed(): void {
--- src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.view-model.ts
+++ src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.view-model.ts
@@ -1,3 +1,13 @@
 export class AddTodoListTaskViewModel {
+  private todoListIdValue: number | string | undefined;
+
+  public get todoListId(): number | string {
+    return this.todoListIdValue ?? 0;
+  }
+
+  public set todoListId(todoListId: number | string) {
+    this.todoListIdValue = todoListId;
+  }
+
   public add(): void {}
 }
