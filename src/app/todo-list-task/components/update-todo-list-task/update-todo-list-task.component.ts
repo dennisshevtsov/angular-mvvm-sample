c47120ffcdb9bf0582c66bca6890f7db5d1ddb12
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-03-03 10:36:11 +0300

    use links in update todo list task comp

--- src/app/todo-list-task/components/update-todo-list-task/update-todo-list-task.component.ts
+++ src/app/todo-list-task/components/update-todo-list-task/update-todo-list-task.component.ts
@@ -1,7 +1,11 @@
-import { Component,              } from '@angular/core';
+import { Component, OnInit,      } from '@angular/core';
 import { FormBuilder, FormGroup, } from '@angular/forms';
+import { ActivatedRoute,         } from '@angular/router';
 
-import { FormComponentBase, } from 'src/app/core';
+import { FormComponentBase,
+         TodoListTaskLinks,
+         TODO_LIST_ROUTE_ID_PARAMETER, } from 'src/app/core';
+import { UpdateTodoListTaskViewModel,  } from './update-todo-list-task.view-model';
 
 @Component({
   templateUrl: './update-todo-list-task.component.html',
@@ -9,15 +13,31 @@ import { FormComponentBase, } from 'src/app/core';
     './update-todo-list-task.component.scss',
   ],
 })
-export class UpdateTodoListTaskComponent extends  FormComponentBase {
+export class UpdateTodoListTaskComponent
+  extends  FormComponentBase
+  implements OnInit {
   public constructor(
+    public readonly vm: UpdateTodoListTaskViewModel,
+
     private readonly fb: FormBuilder,
+    private readonly route: ActivatedRoute,
+    private readonly links: TodoListTaskLinks,
   ) {
     super();
   }
 
-  public get backLink(): string {
-    return '';
+  public ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const todoListId = params.get(TODO_LIST_ROUTE_ID_PARAMETER);
+
+      if (todoListId) {
+        this.vm.todoListId = todoListId;
+      }
+    });
+  }
+
+  public get backLink(): any[] {
+    return this.links.searchTodoListTasksLink(this.vm.todoListId);
   }
 
   public onOkPressed(): void {}
