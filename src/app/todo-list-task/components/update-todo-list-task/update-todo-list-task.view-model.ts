1b67a0fa4eac541c630939279ebe28c4f725de2d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-03-03 10:31:34 +0300

    add todo list id prop to update todo list task vm

--- src/app/todo-list-task/components/update-todo-list-task/update-todo-list-task.view-model.ts
+++ src/app/todo-list-task/components/update-todo-list-task/update-todo-list-task.view-model.ts
@@ -1,2 +1,11 @@
 export class UpdateTodoListTaskViewModel {
+  private todoListIdValue: number | string | undefined;
+
+  public get todoListId(): number | string {
+    return this.todoListIdValue ?? 0;
+  }
+
+  public set todoListId(todoListId: number | string) {
+    this.todoListIdValue = todoListId;
+  }
 }
