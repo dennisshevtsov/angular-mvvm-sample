381d41d56e691d40f809551b08a305b012415086
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-03-14 11:04:21 +0300

    remove error comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule,             } from '@angular/core';
 import { RouterModule, Routes, } from '@angular/router';
 
-import { ErrorComponent,           } from './components';
-import { ERROR_ROUTE_NEW_FRAGMENT, } from './core';
-
 const routes: Routes = [
-  {
-    path: ERROR_ROUTE_NEW_FRAGMENT,
-    component: ErrorComponent,
-  },
   {
     path: '',
     pathMatch: 'full',
--- src/app/components/error/error.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, } from '@angular/core';
-
-@Component({
-  templateUrl: './error.component.html',
-  styleUrls: [
-    './error.component.scss',
-  ],
-})
-export class ErrorComponent { }
