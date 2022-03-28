b1f940befa2a01df01f75dec5c4eeda97635c28f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-03-28 10:27:17 +0300

    add value accessor for todo list task period comp

--- src/app/todo-list-task/components/todo-list-task-period/todo-list-task-period.component.ts
+++ src/app/todo-list-task/components/todo-list-task-period/todo-list-task-period.component.ts
@@ -1,11 +1,42 @@
-import { Component, } from '@angular/core';
+import { Component,            } from '@angular/core';
+import { ControlValueAccessor,
+         NG_VALUE_ACCESSOR,    } from '@angular/forms';
+
+import { TodoListTaskDateDto, } from 'src/app/todo-list-task/api';
 
 @Component({
   selector: 'todo-list-task-period',
   templateUrl: './todo-list-task-period.component.html',
   styleUrls: [
     './todo-list-task-period.component.scss',
-  ]
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: TodoListTaskPeriodComponent,
+    }
+  ],
 })
-export class TodoListTaskPeriodComponent {
+export class TodoListTaskPeriodComponent implements ControlValueAccessor {
+  private period   : undefined | TodoListTaskDateDto;
+  private onChange : any;
+  private onTouched: any;
+  private disabled : boolean = false;
+
+  public writeValue(obj: any): void {
+    this.period = obj;
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 }
