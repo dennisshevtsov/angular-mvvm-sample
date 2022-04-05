bc440ef0ecfb1be20bb12dba0bc96e4d6850229b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-04-05 10:43:50 +0300

    remove formatting date period in update todo list task comp

--- src/app/todo-list-task/components/update-todo-list-task/update-todo-list-task.component.ts
+++ src/app/todo-list-task/components/update-todo-list-task/update-todo-list-task.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy,
          OnInit, ViewChild,        } from '@angular/core';
-import { FormBuilder, FormControl,
-         FormGroup, Validators,    } from '@angular/forms';
+import { FormBuilder, FormGroup,
+         Validators,               } from '@angular/forms';
 import { ActivatedRoute,           } from '@angular/router';
 
 import { Subscription, } from 'rxjs';
@@ -11,6 +11,7 @@ import { Formatter, FormComponentBase,
          TodoListTaskLinks,
          TODO_LIST_ROUTE_ID_PARAMETER,
          TODO_LIST_TASK_ROUTE_ID_PARAMETER, } from 'src/app/core';
+import { TodoListTaskDateDto,               } from 'src/app/todo-list-task/api';
 import { UpdateTodoListTaskViewModel,       } from './update-todo-list-task.view-model';
 
 @Component({
@@ -55,19 +56,8 @@ export class UpdateTodoListTaskComponent
           this.vm.todoListTaskId = todoListTaskId;
 
           const observer = {
-            complete: () => {
-              this.form.setValue({
-                'title': this.vm.task.title,
-                'description': this.vm.task.description,
-                'date': {
-                  'day': this.formatter.toLocalDate(this.vm.task.date.day),
-                  'fullDay': this.vm.task.date.fullDay,
-                  'start': this.formatter.toLocalTime(this.vm.task.date.start),
-                  'end': this.formatter.toLocalTime(this.vm.task.date.end),
-                },
-              });
-            },
-            error: () => this.page.showError('An error occured.'),
+            complete: () => this.form.setValue(this.vm.task),
+            error   : () => this.page.showError('An error occured.'),
           };
 
           this.subscription.add(
@@ -110,18 +100,7 @@ export class UpdateTodoListTaskComponent
     return this.fb.group({
       'title': this.fb.control('', Validators.required),
       'description': '',
-      'date': this.buildTimePeriodControl(),
+      'date': new TodoListTaskDateDto(),
     });
-  }
-
-  private buildTimePeriodControl(): FormControl {
-    const controlConfig = {
-      'day': '',
-      'fullDay': false,
-      'start': '',
-      'end': '',
-    };
-
-    return this.fb.control(controlConfig);
   }
 }
