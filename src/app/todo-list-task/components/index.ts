a97336bdb2ee20a9031a060babb6083b2bb7d37c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-04-08 10:38:06 +0300

    clean todo list task module

--- src/app/todo-list-task/components/index.ts
+++ src/app/todo-list-task/components/index.ts
@@ -1,3 +1,5 @@
 export * from './add-todo-list-task/add-todo-list-task.component';
 export * from './search-todo-list-tasks/search-todo-list-tasks.component';
+export * from './todo-list-task/todo-list-task.component';
+export * from './todo-list-task-period/todo-list-task-period.component';
 export * from './update-todo-list-task/update-todo-list-task.component';
--- src/app/todo-list-task/todo-list-task.module.ts
+++ src/app/todo-list-task/todo-list-task.module.ts
@@ -5,20 +5,20 @@ import { ReactiveFormsModule,  } from '@angular/forms';
 import { CoreModule,                   } from '../core';
 import { AddTodoListTaskComponent,
          SearchTodoListTasksComponent,
+         TodoListTaskPeriodComponent,
+         TodoListTaskComponent,
          UpdateTodoListTaskComponent,  } from './components';
 import { TodoListTaskTimePipe,         } from './pipes';
 import { TodoListTaskRoutingModule,    } from './todo-list-task-routing.module';
-import { TodoListTaskPeriodComponent } from './components/todo-list-task-period/todo-list-task-period.component';
-import { TodoListTaskComponent } from './components/todo-list-task/todo-list-task.component';
 
 @NgModule({
   declarations: [
     AddTodoListTaskComponent,
     SearchTodoListTasksComponent,
-    UpdateTodoListTaskComponent,
-    TodoListTaskTimePipe,
     TodoListTaskPeriodComponent,
     TodoListTaskComponent,
+    UpdateTodoListTaskComponent,
+    TodoListTaskTimePipe,
   ],
   imports: [
     CommonModule,
