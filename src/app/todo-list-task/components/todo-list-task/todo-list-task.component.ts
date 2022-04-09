3a5e9bbfb9703d3ccfacb9e53d66cda257998c1a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-04-09 10:20:30 +0300

    impl todo list task comp

--- src/app/todo-list-task/components/todo-list-task/todo-list-task.component.ts
+++ src/app/todo-list-task/components/todo-list-task/todo-list-task.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,              } from '@angular/core';
+import { FormBuilder, FormGroup,
+         Validators,             } from '@angular/forms';
+
+import { AppClock, FormComponentBase,
+         MILLISECONDS_IN_HOUR,        } from 'src/app/core';
+import { TodoListTaskDateDto,         } from 'src/app/todo-list-task/api';
 
 @Component({
-  selector: 'app-todo-list-task',
+  selector: 'todo-list-task',
   templateUrl: './todo-list-task.component.html',
-  styleUrls: ['./todo-list-task.component.scss']
+  styleUrls: [
+    './todo-list-task.component.scss',
+  ],
 })
-export class TodoListTaskComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class TodoListTaskComponent
+  extends FormComponentBase {
+  public constructor(
+    private readonly fb   : FormBuilder,
+    private readonly clock: AppClock,
+  ) {
+    super();
   }
 
+  protected buildForm(): FormGroup {
+    const now = this.clock.now();
+
+    return this.fb.group({
+      'title': this.fb.control('', Validators.required),
+      'description': '',
+      'date': this.buildDefaultTimePeriod(now),
+    });
+  }
+
+  private buildDefaultTimePeriod(now: number): TodoListTaskDateDto {
+    const start = now;
+    const end = start + MILLISECONDS_IN_HOUR;
+
+    return new TodoListTaskDateDto(now, false, start, end);
+  }
 }
