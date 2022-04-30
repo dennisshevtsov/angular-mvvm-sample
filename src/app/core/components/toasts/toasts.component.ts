f96019d538b4dd68bee83e12f16bc51bba810a1b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-04-30 11:18:46 +0300

    impl toasts comp

--- src/app/core/components/toasts/toasts.component.ts
+++ src/app/core/components/toasts/toasts.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild, ViewContainerRef, } from '@angular/core';
 
+import { ToastComponent, } from '../toast/toast.component';
+
 @Component({
   selector: 'app-toasts',
   templateUrl: './toasts.component.html',
@@ -8,8 +10,14 @@ import { Component, ViewChild, ViewContainerRef, } from '@angular/core';
   ],
 })
 export class ToastsComponent {
-  @ViewChild('container')
-  private viewContainerRef!: ViewContainerRef;
+  public constructor(
+    private readonly viewContainerRef: ViewContainerRef,
+  ) {}
 
-  public push(title: string, message: string) {}
+  public push(title: string, message: string) {
+    const component = this.viewContainerRef.createComponent(ToastComponent);
+
+    component.instance.title = title;
+    component.instance.message = message;
+  }
 }
