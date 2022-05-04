66b418189482de1f55865af008da195d8a42f63d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-05-04 11:27:13 +0300

    remove msgs from page comp

--- src/app/core/components/page/page.component.ts
+++ src/app/core/components/page/page.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, TemplateRef, } from '@angular/core';
-import { Observable, of } from 'rxjs';
 
 import { TodoListLinks, } from 'src/app/core/navigation';
 
@@ -17,54 +16,11 @@ export class PageComponent {
   @Input()
   public body: TemplateRef<any> | null = null;
 
-  private errorValue  : undefined | string;
-  private messageValue: undefined | string;
-
   public constructor(
     private readonly links: TodoListLinks,
   ) {}
 
   public get homeLink(): any[] {
     return this.links.searchTodoListsLink();
-  }
-
-  public get hasError(): boolean {
-    return !!this.errorValue;
-  }
-
-  public get error(): string {
-    return this.errorValue ?? '';
-  }
-
-  public get hasMessage(): boolean {
-    return !!this.messageValue;
-  }
-
-  public get message(): string {
-    return this.messageValue ?? '';
-  }
-
-  public showError(error: string): void {
-    this.errorValue = error;
-  }
-
-  public showMessage(message: string): Promise<void> {
-    return Promise.resolve(null)
-                  .then(() => {
-                    this.messageValue = message;
-                  });
-  }
-
-  public clearError(): void {
-    this.errorValue = '';
-  }
-
-  public clearMessage(): void {
-    this.messageValue = '';
-  }
-
-  public clear(): void {
-    this.clearError();
-    this.clearMessage();
   }
 }
