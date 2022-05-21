2eac3ec7719f23fe6f7de4fa2950e243bf640005
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-05-21 11:56:11 +0300

    impl time control

--- src/app/core/components/time/time.component.ts
+++ src/app/core/components/time/time.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, } from '@angular/core';
+
+import { HOURS_IN_DAY,
+         MILLISECONDS_IN_HOUR,
+         MILLISECONDS_IN_MENUTE, } from '../../date';
+
+export const DEFAULT_MENUTES_STEP = 15;
 
 @Component({
-  selector: 'app-time',
+  selector: 'time',
   templateUrl: './time.component.html',
-  styleUrls: ['./time.component.scss']
+  styleUrls: [
+    './time.component.scss',
+  ],
 })
-export class TimeComponent implements OnInit {
+export class TimeComponent {
+  private menutesStep: number;
+  private time       : number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public constructor() {
+    this.menutesStep = DEFAULT_MENUTES_STEP;
+    this.time = 0;
   }
 
+  @Input()
+  public set step(value: number) {
+    this.menutesStep = value;
+  }
+
+  @Input()
+  public set value(value: number) {
+    this.time = value;
+  }
+
+  public get hours(): number {
+    return this.time / MILLISECONDS_IN_HOUR >> 0;
+  }
+
+  public get minutes(): number {
+    return this.time % MILLISECONDS_IN_HOUR / MILLISECONDS_IN_MENUTE >> 0;
+  }
+
+  public increaseHours() {
+    let value = this.time;
+
+    value = value + 1;
+    value = value % HOURS_IN_DAY;
+
+    this.time = value;
+  }
+
+  public decreaseHours() {
+    let value = this.time;
+
+    value = value - 1;
+
+    if (value < 0) {
+      value = value + HOURS_IN_DAY;
+    }
+
+    this.time = value;
+  }
+
+  public increaseMenutes() {
+    let value = this.time;
+
+    value = value + MILLISECONDS_IN_MENUTE * this.menutesStep;
+    value = value % MILLISECONDS_IN_HOUR;
+
+    this.time = value;
+  }
+
+  public decreaseMenutes() {
+    let value = this.time;
+
+    value = value - MILLISECONDS_IN_MENUTE * this.menutesStep;
+
+    if (value < 0) {
+      value = value + MILLISECONDS_IN_HOUR;
+    }
+
+    this.time = value;
+  }
 }
--- src/app/core/date/app-clock.ts
+++ src/app/core/date/app-clock.ts
@@ -1,7 +1,10 @@
 import { Injectable, } from '@angular/core';
 
+export const MILLISECONDS_IN_SECOND = 1 *  1000;
 export const MILLISECONDS_IN_MENUTE = 1 * 60 * 1000;
 export const MILLISECONDS_IN_HOUR   = 1 * 60 * MILLISECONDS_IN_MENUTE;
+
+export const HOURS_IN_DAY = 24;
 
 @Injectable({
   providedIn: 'root',
