3246209535edc21b9aeeaeea1b109f42df2099d8
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-06-02 10:35:30 +0300

    start impl date comp

--- src/app/core/components/date/date.component.ts
+++ src/app/core/components/date/date.component.ts
@@ -1,4 +1,6 @@
-import { Component, } from '@angular/core';
+import { Component,            } from '@angular/core';
+import { ControlValueAccessor,
+         NG_VALUE_ACCESSOR,    } from '@angular/forms';
 
 @Component({
   selector: 'date',
@@ -6,6 +8,38 @@ import { Component, } from '@angular/core';
   styleUrls: [
     './date.component.scss',
   ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: DateComponent,
+    },
+  ],
 })
-export class DateComponent {
+export class DateComponent implements ControlValueAccessor {
+  private value   : number;
+  private onChange: (value: any) => void;
+
+  public constructor() {
+    this.value = 0;
+    this.onChange = () => {};
+  }
+
+  public get date(): number {
+    return this.value;
+  }
+
+  public writeValue(value: number): void {
+    this.value = value;
+  }
+
+  public registerOnChange(fn: (value: any) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+  }
+
+  public setDisabledState(disabled: boolean): void {
+  }
 }
