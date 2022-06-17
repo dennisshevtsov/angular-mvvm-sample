aaba27135f081c989cb61b4f303d27b1cc3a5c12
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-06-17 11:39:20 +0300

    move todo list tests to own module

--- src/app/todo-list/components/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/components/todo-list/todo-list.component.spec.ts
@@ -4,93 +4,95 @@ import { ReactiveFormsModule, Validators, } from '@angular/forms';
 import { AddTodoListRequestDto } from 'src/app/todo-list/api';
 import { TodoListComponent,    } from './todo-list.component';
 
-let component: TodoListComponent;
-let fixture  : ComponentFixture<TodoListComponent>;
+describe('TodoListComponent', () => {
+  let component: TodoListComponent;
+  let fixture  : ComponentFixture<TodoListComponent>;
 
-beforeEach(() => {
-  TestBed.configureTestingModule({
-    declarations: [
-      TodoListComponent,
-    ],
-    imports: [
-      ReactiveFormsModule,
-    ],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TodoListComponent,
+      ],
+      imports: [
+        ReactiveFormsModule,
+      ],
+    });
+
+    fixture = TestBed.createComponent(TodoListComponent);
+    component = fixture.componentInstance;
   });
 
-  fixture = TestBed.createComponent(TodoListComponent);
-  component = fixture.componentInstance;
-});
+  it('should contain initialized form', () => {
+    const todoList = new AddTodoListRequestDto(
+      'test title', 'test description');
 
-it('should contain initialized form', () => {
-  const todoList = new AddTodoListRequestDto(
-    'test title', 'test description');
+    component.todoList = todoList;
+    component.ngOnInit();
 
-  component.todoList = todoList;
-  component.ngOnInit();
+    expect(component.form).not.toBeUndefined();
+    expect(component.form).not.toBeNull();
 
-  expect(component.form).not.toBeUndefined();
-  expect(component.form).not.toBeNull();
+    const titleControl = component.form.get('title');
 
-  const titleControl = component.form.get('title');
+    expect(titleControl).not.toBeNull();
+    expect(titleControl!.hasValidator(Validators.required)).toBeTrue();
+    expect(titleControl!.value).toBe(todoList.title);
 
-  expect(titleControl).not.toBeNull();
-  expect(titleControl!.hasValidator(Validators.required)).toBeTrue();
-  expect(titleControl!.value).toBe(todoList.title);
+    const descriptionControl = component.form.get('description');
 
-  const descriptionControl = component.form.get('description');
+    expect(descriptionControl).not.toBeNull();
+    expect(descriptionControl!.validator).toBeNull();
+    expect(descriptionControl!.value).toBe(todoList.description);
+  });
 
-  expect(descriptionControl).not.toBeNull();
-  expect(descriptionControl!.validator).toBeNull();
-  expect(descriptionControl!.value).toBe(todoList.description);
-});
+  it('should validate form', () => {
+    component.todoList = new AddTodoListRequestDto();
+    component.ngOnInit();
 
-it('should validate form', () => {
-  component.todoList = new AddTodoListRequestDto();
-  component.ngOnInit();
+    const titleControl = component.form.get('title')!;
+    const descriptionControl = component.form.get('description')!;
 
-  const titleControl = component.form.get('title')!;
-  const descriptionControl = component.form.get('description')!;
+    titleControl.setValue('');
+    descriptionControl.setValue('');
 
-  titleControl.setValue('');
-  descriptionControl.setValue('');
+    component.validateForm();
 
-  component.validateForm();
+    expect(component.form.valid)
+      .withContext('form shoul be invalid')
+      .toBeFalse();
 
-  expect(component.form.valid)
-    .withContext('form shoul be invalid')
-    .toBeFalse();
+    expect(titleControl.valid)
+      .withContext('description should not be valid')
+      .toBeFalse();
+    expect(titleControl.errors)
+      .withContext('title errors should not be empty')
+      .not.toBeNull();
 
-  expect(titleControl.valid)
-    .withContext('description should not be valid')
-    .toBeFalse();
-  expect(titleControl.errors)
-    .withContext('title errors should not be empty')
-    .not.toBeNull();
+    const titleErrorKeys = Object.keys(titleControl.errors!);
 
-  const titleErrorKeys = Object.keys(titleControl.errors!);
+    expect(titleErrorKeys.length)
+      .withContext('title errors should contains only one error')
+      .toBe(1);
+    expect(titleErrorKeys[0])
+      .withContext('title errors should contains only required error')
+      .toBe('required');
 
-  expect(titleErrorKeys.length)
-    .withContext('title errors should contains only one error')
-    .toBe(1);
-  expect(titleErrorKeys[0])
-    .withContext('title errors should contains only required error')
-    .toBe('required');
+    expect(descriptionControl.valid)
+      .withContext('description should be valid')
+      .toBeTrue();
 
-  expect(descriptionControl.valid)
-    .withContext('description should be valid')
-    .toBeTrue();
+    titleControl.setValue('test title');
 
-  titleControl.setValue('test title');
+    component.validateForm();
 
-  component.validateForm();
-
-  expect(component.form.valid)
-    .withContext('form should be valid')
-    .toBeTrue();
-  expect(titleControl.valid)
-    .withContext('title should be valid')
-    .toBeTrue();
-  expect(descriptionControl.valid)
-    .withContext('description should be valid')
-    .toBeTrue();
+    expect(component.form.valid)
+      .withContext('form should be valid')
+      .toBeTrue();
+    expect(titleControl.valid)
+      .withContext('title should be valid')
+      .toBeTrue();
+    expect(descriptionControl.valid)
+      .withContext('description should be valid')
+      .toBeTrue();
+  });
 });
