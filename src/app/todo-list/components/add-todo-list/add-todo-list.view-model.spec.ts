529cf7b3003805d067428e3f107f47da0e55df58
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-06-19 11:45:13 +0300

    start adding test for add todo list vm

---
+++ src/app/todo-list/components/add-todo-list/add-todo-list.view-model.spec.ts
@@ -0,0 +1,41 @@
+import { inject, TestBed, } from '@angular/core/testing';
+
+import { of, } from 'rxjs';
+
+import { AddTodoListResponseDto, TodoListService, } from 'src/app/todo-list/api';
+import { AddTodoListViewModel, } from './add-todo-list.view-model';
+
+describe('AddTodoListViewModel', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AddTodoListViewModel,
+        {
+          provide: TodoListService,
+          useValue: jasmine.createSpyObj(
+            TodoListService,
+            [
+              'addTodoList',
+            ]),
+        },
+      ]
+    });
+  });
+
+  it('should call addTodoList method', inject(
+    [
+      AddTodoListViewModel,
+      TodoListService,
+    ],
+    (vm: AddTodoListViewModel,
+     service: jasmine.SpyObj<TodoListService>) => {
+    service.addTodoList.and.returnValue(
+      of(new AddTodoListResponseDto('test id')));
+
+    vm.add();
+
+    expect(service.addTodoList.calls.count())
+      .withContext('should call addTodoLIst method once')
+      .toBe(1);
+  }))
+});
