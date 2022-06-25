83bf4b853ae55a2bb1cf18dba78938095ef0a14c
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-06-25 20:01:26 +0300

    add test for search todo lists comp

--- src/app/todo-list/components/search-todo-lists/search-todo-lists.component.spec.ts
+++ src/app/todo-list/components/search-todo-lists/search-todo-lists.component.spec.ts
@@ -1,11 +1,27 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, inject, TestBed, } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { of } from 'rxjs';
 
-import { CoreModule, TodoListLinks,
+import { of, } from 'rxjs';
+
+import { CoreModule, PageComponent, TodoListLinks,
          TodoListTaskLinks,                } from 'src/app/core';
+import { SearchTodoListsRecordResponseDto } from '../../api';
 import { SearchTodoListsComponent,         } from './search-todo-lists.component';
 import { SearchTodoListsViewModel,         } from './search-todo-lists.view-model';
+
+@Component({
+  selector: 'app-modal',
+})
+class ModalComponentMock {
+  public show(): void {}
+}
+
+@Component({
+  selector: 'app-toasts',
+})
+class ToastsComponentMock {}
 
 describe('SearchTodoListsComponent', () => {
   let vm               : jasmine.SpyObj<SearchTodoListsViewModel>;
@@ -13,6 +29,12 @@ describe('SearchTodoListsComponent', () => {
   let todoListTaskLinks: jasmine.SpyObj<TodoListTaskLinks>;
 
   let fixture: ComponentFixture<SearchTodoListsComponent>;
+
+  const recordDto = new SearchTodoListsRecordResponseDto(
+    1,
+    'test todo list title 0',
+    'test todo list description 0',
+  );
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -25,11 +47,33 @@ describe('SearchTodoListsComponent', () => {
       ],
     });
 
+    TestBed.overrideModule(
+      CoreModule,
+      {
+        set: {
+          declarations: [
+            PageComponent,
+            ModalComponentMock,
+            ToastsComponentMock,
+          ],
+          exports: [
+            PageComponent,
+            ModalComponentMock,
+            ToastsComponentMock,
+          ],
+        }
+      }
+    )
+
     vm = jasmine.createSpyObj(
-      SearchTodoListsViewModel,
+      'SearchTodoListsViewModel',
       [
         'search',
         'delete',
+      ],
+      [
+        'todoLists',
+        'selected',
       ]);
 
     vm.search.and.returnValue(of(void 0));
@@ -41,7 +85,7 @@ describe('SearchTodoListsComponent', () => {
       });
 
     todoListLinks = jasmine.createSpyObj(
-      TodoListLinks,
+      'TodoListLinks',
       [
         'addTodoListLink',
         'updateTodoListLink',
@@ -49,6 +93,25 @@ describe('SearchTodoListsComponent', () => {
       ]);
 
     todoListLinks.searchTodoListsLink.and.returnValue([]);
+
+    const todoListsPropertyDescriptor = Object.getOwnPropertyDescriptor(vm, 'todoLists')!;
+    const todoListsSpy = todoListsPropertyDescriptor.get as jasmine.Spy<() => SearchTodoListsRecordResponseDto[]>;
+
+    const redordDtos = [
+      recordDto,
+      new SearchTodoListsRecordResponseDto(
+        2,
+        'test todo list title 1',
+        'test todo list description 1',
+      ),
+      new SearchTodoListsRecordResponseDto(
+        3,
+        'test todo list title 2',
+        'test todo list description 2',
+      ),
+    ];
+
+    todoListsSpy.and.returnValue(redordDtos);
 
     TestBed.overrideProvider(
       TodoListLinks,
@@ -71,4 +134,28 @@ describe('SearchTodoListsComponent', () => {
     fixture = TestBed.createComponent(SearchTodoListsComponent);
     fixture.detectChanges();
   });
+
+  it('onDeleteOkPressed should be called on delete button click', () => {
+    vm.delete.and.returnValue(of(void 0));
+
+    const selectedPropertyDescriptor = Object.getOwnPropertyDescriptor(vm, 'selected')!;
+    const selectedSpy = selectedPropertyDescriptor.set as jasmine.Spy<(record: SearchTodoListsRecordResponseDto) => void>;
+
+    const modalComponent = fixture.debugElement.query(By.css('app-modal')).componentInstance;
+    const showSpy = spyOn(modalComponent, 'show');
+
+    fixture.debugElement.query(By.css('#btnDelete-1')).nativeElement.dispatchEvent(new Event('click'));
+
+    expect(selectedSpy.calls.count())
+      .withContext('selected should be called once')
+      .toBe(1);
+
+    expect(selectedSpy.calls.first().args[0])
+      .withContext('selected should be called once')
+      .toEqual(recordDto);
+
+    expect(showSpy.calls.count())
+      .withContext('show should be called once')
+      .toBe(1);
+  })
 });
