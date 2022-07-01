2c06feab3a9cb24b000c458f2ce8e3d6e5f7e1e8
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-01 11:02:31 +0300

    start tests for search todo list tasks comp

---
+++ src/app/todo-list-task/components/search-todo-list-tasks/search-todo-list-tasks.component.spec.ts
@@ -0,0 +1,15 @@
+import { fakeAsync, inject, TestBed, } from '@angular/core/testing';
+
+import { SearchTodoListTasksComponent, } from './search-todo-list-tasks.component';
+
+describe('SearchTodoListTasksComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SearchTodoListTasksComponent, ],
+    });
+  });
+
+  it('ngOnInit should populate error', fakeAsync(inject([], () => {
+
+  })));
+});
