3be269e45bea32a8dd811a13b0e48edeb1efd45d
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-04 18:24:35 +0300

    add tests for todo list task comp

---
+++ src/app/todo-list-task/components/todo-list-task/todo-list-task.component.spec.ts
@@ -0,0 +1,67 @@
+import { inject, TestBed,              } from '@angular/core/testing';
+import { FormBuilder, FormGroup,       } from '@angular/forms';
+
+import { Observable, of, Subscription, } from 'rxjs';
+
+import { AddTodoListTaskRequestDto,
+         TodoListTaskDateDto,          } from 'src/app/todo-list-task/api';
+import { TodoListTaskComponent,        } from './todo-list-task.component';
+
+describe('TodoListTaskComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TodoListTaskComponent, ],
+      providers: [
+        {
+          provide: FormBuilder,
+          useValue: jasmine.createSpyObj(FormBuilder, [ 'group', 'control', ]),
+        },
+      ],
+    });
+
+    TestBed.overrideProvider(
+      Subscription,
+      {
+        useValue: jasmine.createSpyObj(Subscription, [ 'add', 'unsubscribe', ]),
+      });
+  });
+
+  it('ngOnInit should initialize form', inject(
+    [ Subscription, FormBuilder, ],
+    (subSpy: jasmine.SpyObj<Subscription>,
+     fbSpy : jasmine.SpyObj<FormBuilder>) => {
+    const requestDto = new AddTodoListTaskRequestDto(
+      'test todo list id',
+      'test todo list title',
+      'test todo list description',
+      new TodoListTaskDateDto());
+
+    const formSpy: jasmine.SpyObj<FormGroup> =
+      jasmine.createSpyObj('FormGroup', [ 'setValue', 'get', ], [ 'valueChanges', ]);
+
+    fbSpy.group.and.returnValue(formSpy);
+
+    const descs = Object.getOwnPropertyDescriptors(formSpy);
+    const vcSpy = descs.valueChanges.get! as jasmine.Spy<() => Observable<any>>;
+
+    vcSpy.and.returnValue(of(requestDto));
+
+    const fixture = TestBed.createComponent(TodoListTaskComponent);
+
+    fixture.componentInstance.task = requestDto;
+
+    fixture.detectChanges();
+
+    expect(formSpy.setValue.calls.count())
+      .withContext('setValue should be called')
+      .toBe(1);
+
+    expect(vcSpy.calls.count())
+      .withContext('valueChanges should be subscribed')
+      .toBe(1);
+
+    expect(subSpy.add.calls.count())
+      .withContext('add should be called')
+      .toBe(1);
+  }));
+});
