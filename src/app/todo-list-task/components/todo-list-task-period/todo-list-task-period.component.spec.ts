af8c4a7ac20a8d384cc8905c6be3211f7184178f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-06 11:54:55 +0300

    add tests for todo list task period comp

--- src/app/todo-list-task/components/todo-list-task-period/todo-list-task-period.component.spec.ts
+++ src/app/todo-list-task/components/todo-list-task-period/todo-list-task-period.component.spec.ts
@@ -4,6 +4,7 @@ import { AbstractControl, FormBuilder,
 
 import { Observable, of, Subscription, } from 'rxjs';
 
+import { TodoListTaskDateDto,          } from 'src/app/todo-list-task/api';
 import { TodoListTaskPeriodComponent,  } from './todo-list-task-period.component';
 
 describe('TodoListTaskPeriodComponent', () => {
@@ -13,7 +14,7 @@ describe('TodoListTaskPeriodComponent', () => {
     const valueChangesSpy = controlSpyDescs.valueChanges.get! as jasmine.Spy<() => Observable<any>>;
     valueChangesSpy.and.returnValue(of({}));
 
-    const formSpy : jasmine.SpyObj<FormGroup> = jasmine.createSpyObj('FormGroup', [ 'get', ], [ 'value', ]);
+    const formSpy : jasmine.SpyObj<FormGroup> = jasmine.createSpyObj('FormGroup', [ 'get', 'setValue', ], [ 'value', ]);
     const formSpyDescs = Object.getOwnPropertyDescriptors(formSpy);
     const valueSpy = formSpyDescs.value.get! as jasmine.Spy<() => any>;
 
@@ -27,6 +28,7 @@ describe('TodoListTaskPeriodComponent', () => {
       declarations: [ TodoListTaskPeriodComponent, ],
       providers: [
         { provide: FormBuilder    , useValue: fbSpy,      },
+        { provide: FormGroup      , useValue: formSpy,    },
         { provide: AbstractControl, useValue: controlSpy, }, ],
     });
 
@@ -55,10 +57,7 @@ describe('TodoListTaskPeriodComponent', () => {
   }));
 
   it('ngOnDestroy should unsubscribe',
-     inject(
-      [Subscription, AbstractControl],
-      (subSpy    : jasmine.SpyObj<Subscription>,
-       controlSpy: jasmine.SpyObj<AbstractControl>) => {
+     inject([Subscription], (subSpy: jasmine.SpyObj<Subscription>) => {
     const fixture = TestBed.createComponent(TodoListTaskPeriodComponent);
 
     fixture.detectChanges();
@@ -74,6 +73,27 @@ describe('TodoListTaskPeriodComponent', () => {
 
     expect(subSpy.unsubscribe.calls.count())
       .withContext('add should be called')
+      .toBe(1);
+  }));
+
+  it('writeValue should populate form',
+     inject([FormGroup], (formSpy: jasmine.SpyObj<FormGroup>) => {
+    const fixture = TestBed.createComponent(TodoListTaskPeriodComponent);
+
+    fixture.detectChanges();
+
+    fixture.componentInstance.writeValue(null!);
+
+    expect(formSpy.setValue.calls.count())
+      .withContext('setValue should not be called')
+      .toBe(0);
+
+    const periodDto = new TodoListTaskDateDto(100000, false, 200000, 300000)
+
+    fixture.componentInstance.writeValue(periodDto);
+
+    expect(formSpy.setValue.calls.count())
+      .withContext('setValue should be called')
       .toBe(1);
   }));
 });
