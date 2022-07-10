d0b2418f4bcabd6923c96e11110c0559de68a948
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-10 12:29:17 +0300

    add test for todo list task time pipe

--- src/app/todo-list-task/pipes/todo-list-task-time.pipe.spec.ts
+++ src/app/todo-list-task/pipes/todo-list-task-time.pipe.spec.ts
@@ -17,7 +17,7 @@ describe('TodoListTaskTimePipe', () => {
     });
   });
 
-  it('transform should return date', inject(
+  it('transform should return day', inject(
     [
       Formatter,
       TodoListTaskTimePipe,
@@ -42,6 +42,45 @@ describe('TodoListTaskTimePipe', () => {
         .toBe(1);
 
       expect(formatterSpy.toLocalDate.calls.first().args[0])
+        .withContext('toLocalDate should be called with correct params')
+        .toBe(unformatted.day);
+  }));
+
+  it('transform should return full formatted date', inject(
+    [
+      Formatter,
+      TodoListTaskTimePipe,
+    ],
+    (
+      formatterSpy: jasmine.SpyObj<Formatter>,
+      pipe        : TodoListTaskTimePipe,
+    ) => {
+      const date = 'day';
+      const time = 'time';
+
+      formatterSpy.toLocalDate.and.returnValue(date);
+      formatterSpy.toLocalTime.and.returnValue(time);
+
+      const unformatted = new TodoListTaskDateDto(1000, false, 2000, 3000);
+      const formatted = pipe.transform(unformatted);
+
+      expect(formatted)
+        .withContext('transform should return date')
+        .toBe('day time-time');
+
+      expect(formatterSpy.toLocalDate.calls.count())
+        .withContext('toLocalDate should be called')
+        .toBe(1);
+
+      expect(formatterSpy.toLocalTime.calls.count())
+        .withContext('toLocalTime should be called')
+        .toBe(2);
+
+      expect(formatterSpy.toLocalDate.calls.first().args[0])
+        .withContext('toLocalDate should be called with correct params')
+        .toBe(unformatted.day);
+
+      expect(formatterSpy.toLocalDate.calls.mostRecent().args[0])
         .withContext('toLocalDate should be called with correct params')
         .toBe(unformatted.day);
   }));
