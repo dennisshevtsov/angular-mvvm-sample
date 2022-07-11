101c7c57e7aef51a19b13b0a63df9c54825e3ee2
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-11 20:19:18 +0300

    add test for time period validator

--- src/app/todo-list-task/validators/time-period-validator.spec.ts
+++ src/app/todo-list-task/validators/time-period-validator.spec.ts
@@ -43,4 +43,59 @@ describe('timePeriodValidator', () => {
       .withContext('fullDayControl.value should be called')
       .toBe(1);
   });
+
+  it('timePeriodValidator should not validate pre-populate control', () => {
+    const fullDayControlSpy : jasmine.SpyObj<AbstractControl> = jasmine.createSpyObj('AbstractControl', [], ['value']);
+    const fullDayControlSpyDescs = Object.getOwnPropertyDescriptors(fullDayControlSpy)!;
+    const fullDayPropSpy = fullDayControlSpyDescs.value.get as jasmine.Spy<() => any>;
+
+    fullDayPropSpy.and.returnValue(false);
+
+    const startControlSpy : jasmine.SpyObj<AbstractControl> = jasmine.createSpyObj('AbstractControl', ['setErrors'], []);
+    const endControlSpy : jasmine.SpyObj<AbstractControl> = jasmine.createSpyObj('AbstractControl', ['setErrors'], []);
+
+    const timePeriodControlSpy: jasmine.SpyObj<AbstractControl> = jasmine.createSpyObj('AbstractControl', ['get'], ['pristine','touched','dirty']);
+    const timePeriodControlSpyDescs = Object.getOwnPropertyDescriptors(timePeriodControlSpy)!;
+    const timePeriodPristinePropSpy = timePeriodControlSpyDescs.pristine.get as jasmine.Spy<() => boolean>;
+    const timePeriodTouchedPropSpy = timePeriodControlSpyDescs.touched.get as jasmine.Spy<() => boolean>;
+    const timePeriodDirtyPropSpy = timePeriodControlSpyDescs.dirty.get as jasmine.Spy<() => boolean>;
+
+    timePeriodPristinePropSpy.and.returnValue(true);
+    timePeriodTouchedPropSpy.and.returnValue(false);
+    timePeriodDirtyPropSpy.and.returnValue(false);
+
+    timePeriodControlSpy.get.and.callFake(name => {
+      if (name == 'fullDay') {
+        return fullDayControlSpy;
+      }
+
+      if (name == 'start') {
+        return startControlSpy;
+      }
+
+      if (name == 'end') {
+        return endControlSpy;
+      }
+
+      return null;
+    });
+
+    const errors = timePeriodValidator(timePeriodControlSpy);
+
+    expect(errors)
+      .withContext('timePeriodValidator should return empty errors')
+      .toEqual({});
+
+    expect(timePeriodControlSpy.get.calls.count())
+      .withContext('timePeriodControl.get should be called')
+      .toBe(3);
+
+    expect(fullDayPropSpy.calls.count())
+      .withContext('fullDayControl.value should be called')
+      .toBe(1);
+
+    expect(timePeriodPristinePropSpy.calls.count())
+      .withContext('timePeriod.pristine should be called')
+      .toBe(1);
+  });
 });
