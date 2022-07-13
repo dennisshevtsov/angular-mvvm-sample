561f8c2dcf3b5a808723245429f0ff05cf99b96f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-13 11:04:39 +0300

    add test for date comp

--- src/app/core/components/date/date.component.spec.ts
+++ src/app/core/components/date/date.component.spec.ts
@@ -38,7 +38,40 @@ describe('DateComponent', () => {
         .toBeGreaterThan(1);
 
       expect(formatterSpy.toLocalDate.calls.first().args[0])
-        .withContext('toLocalDate should take correct params')
+        .withContext('toLocalDate should take correct param')
         .toBe(numberDate);
+  }));
+
+  it('value should call onChange and onTouched', inject(
+    [Formatter],
+    (formatterSpy: jasmine.SpyObj<Formatter>) => {
+      const numberDate = 10000;
+      const formattedDate = 'formatted date';
+
+      formatterSpy.fromLocalDate.and.returnValue(numberDate);
+      formatterSpy.toLocalDate.and.returnValue(formattedDate);
+
+      const fixture = TestBed.createComponent(DateComponent);
+
+      fixture.componentInstance.writeValue(numberDate);
+
+      let touched = false;
+      fixture.componentInstance.registerOnTouched(() => touched = true);
+
+      let onChange = (value: any) => {
+        expect(value)
+          .withContext('onChange should take correct param')
+          .toBe(numberDate);
+      };
+      fixture.componentInstance.registerOnChange(onChange);
+      fixture.componentInstance.setDisabledState(true);
+
+      fixture.detectChanges();
+
+      fixture.componentInstance.value = formattedDate;
+
+      expect(formatterSpy.fromLocalDate.calls.count())
+        .withContext('fromLocalDate should not be called')
+        .toBe(0);
   }));
 });
