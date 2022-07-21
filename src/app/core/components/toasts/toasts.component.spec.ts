bc7e53fcbca87dba15d5abd6b82f3aeee45aa9dd
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-21 18:46:28 +0300

    add tests for toasts comp

--- src/app/core/components/toasts/toasts.component.spec.ts
+++ src/app/core/components/toasts/toasts.component.spec.ts
@@ -96,4 +96,66 @@ describe('ToastsComponent', () => {
       .withContext('viewContainerRef.remove should be called')
       .toBe(1);
   }));
+
+  it('info should call toast comp info', inject([Subscription], (subscription: jasmine.SpyObj<Subscription>) => {
+    const component = TestBed.createComponent(ToastsComponent);
+    component.detectChanges();
+
+    const container = (component.componentInstance as any).viewContainerRef;
+    const createComponentSpy = spyOn(container, 'createComponent');
+    const removeSpy = spyOn(container, 'remove');
+
+    const componentSpy = jasmine.createSpyObj([], ['changeDetectorRef', 'instance']);
+    createComponentSpy.and.returnValue(componentSpy);
+
+    const changeDetectorRefDesc = Object.getOwnPropertyDescriptor(componentSpy, 'changeDetectorRef');
+    const changeDetectorRefProp = changeDetectorRefDesc?.get as jasmine.Spy<() => ViewContainerRef>;
+
+    const changeDetectorRefSpy = jasmine.createSpyObj(['detach', 'detectChanges'], []);
+    changeDetectorRefProp.and.returnValue(changeDetectorRefSpy);
+
+    const instanceDesc = Object.getOwnPropertyDescriptor(componentSpy, 'instance');
+    const instanceProp = instanceDesc?.get as jasmine.Spy<() => ComponentRef<ToastComponent>>;
+
+    const instanceSpy = jasmine.createSpyObj(['error', 'info'], []);
+    instanceSpy.hidden = new EventEmitter<void>();
+
+    instanceProp.and.returnValue(instanceSpy);
+
+    component.componentInstance.info('test');
+
+    expect(createComponentSpy.calls.count())
+      .withContext('viewContainerRef.createComponent should be called')
+      .toBe(1);
+
+    expect(changeDetectorRefSpy.detach.calls.count())
+      .withContext('changeDetectorRef.detach should be called')
+      .toBe(1);
+
+    expect(instanceProp.calls.count())
+      .withContext('component.instance should be called twice')
+      .toBe(2);
+
+    expect(subscription.add.calls.count())
+      .withContext('subscription.add should be called')
+      .toBe(1);
+
+    expect(changeDetectorRefSpy.detectChanges.calls.count())
+      .withContext('changeDetectorRef.detectChanges should be called')
+      .toBe(1);
+
+    expect(instanceSpy.error.calls.count())
+      .withContext('instance.error should not be called')
+      .toBe(0);
+
+    expect(instanceSpy.info.calls.count())
+      .withContext('instance.info should be called')
+      .toBe(1);
+
+    instanceSpy.hidden.emit();
+
+    expect(removeSpy.calls.count())
+      .withContext('viewContainerRef.remove should be called')
+      .toBe(1);
+  }));
 });
