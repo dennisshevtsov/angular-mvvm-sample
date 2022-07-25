a4b289b76c58b298e85773b13ff1149197de22b5
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-25 20:27:23 +0300

    add test for todo list links

---
+++ src/app/core/navigation/todo-list.links.spec.ts
@@ -0,0 +1,25 @@
+import { inject, TestBed, } from '@angular/core/testing'
+
+import { TodoListLinks,   } from './todo-list.links'
+
+describe('TodoListLinks', () => {
+  beforeEach(() => TestBed.configureTestingModule({providers: [TodoListLinks]}));
+
+  it('addTodoListLink create URL', inject([TodoListLinks], (links: TodoListLinks) => {
+    expect(links.addTodoListLink())
+      .withContext('addTodoListLink should return URL')
+      .toEqual(['/', 'todo', 'new']);
+  }));
+
+  it('updateTodoListLink create URL', inject([TodoListLinks], (links: TodoListLinks) => {
+    expect(links.updateTodoListLink(123))
+      .withContext('updateTodoListLink should return URL')
+      .toEqual(['/', 'todo', 123]);
+  }));
+
+  it('searchTodoListsLink create URL', inject([TodoListLinks], (links: TodoListLinks) => {
+    expect(links.searchTodoListsLink())
+      .withContext('searchTodoListsLink should return URL')
+      .toEqual(['/', 'todo']);
+  }));
+});
