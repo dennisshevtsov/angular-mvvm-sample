4be62957c67ab4eefd15407904570e05e60f6ffd
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-07-25 19:47:48 +0300

    add test for convert to route func

---
+++ src/app/core/navigation/convert-to-route.spec.ts
@@ -0,0 +1,9 @@
+import { convertToRoute, } from './convert-to-route';
+
+describe('convertToRoute', () => {
+  it ('convertToRoute normolize route', () => {
+    expect(convertToRoute(['/', 'test', 123, 'test']))
+      .withContext('convertToRoute should remove / in beginning')
+      .toBe('test/123/test');
+  });
+});
