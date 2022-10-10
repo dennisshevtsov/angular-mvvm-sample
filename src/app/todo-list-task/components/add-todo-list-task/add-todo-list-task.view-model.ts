f50321b6ea2bbc479e35c60672906689b87d9981
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-10-10 13:38:20 +0300

    remove fullday logic from vm

--- src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.view-model.ts
+++ src/app/todo-list-task/components/add-todo-list-task/add-todo-list-task.view-model.ts
@@ -2,8 +2,6 @@ import { Injectable, } from '@angular/core';
 
 import { map, Observable, } from 'rxjs';
 
-import { AppClock, MILLISECONDS_IN_HOUR,
-         MILLISECONDS_IN_MENUTE,          } from 'src/app/core';
 import { AddTodoListDayTaskRequestDto,
          AddTodoListPeriodTaskRequestDto,
          TodoListTaskService,             } from 'src/app/todo-list-task/api';
@@ -13,10 +11,8 @@ export class AddTodoListTaskViewModel {
   private todoListIdValue    : undefined | string;
   private todoListTaskIdValue: undefined | string;
   private taskValue          : undefined | AddTodoListDayTaskRequestDto | AddTodoListPeriodTaskRequestDto;
-  private fullDayValue       : undefined | boolean;
 
   public constructor(
-    private readonly clock  : AppClock,
     private readonly service: TodoListTaskService,
   ) {}
 
@@ -37,39 +33,8 @@ export class AddTodoListTaskViewModel {
     this.todoListTaskIdValue = todoListTaskId;
   }
 
-  public get fullDay(): boolean {
-    return this.fullDayValue === true;
-  }
-
-  public set fullDay(value: boolean) {
-    if (this.fullDay != value) {
-      if (value) {
-        this.taskValue = new AddTodoListDayTaskRequestDto(
-          this.todoListId,
-          this.task.title,
-          this.task.description,
-          this.clock.now());
-      }
-      else {
-        const now = this.clock.now();
-        const step = 15 * MILLISECONDS_IN_MENUTE;
-        const start = now - (now % step) + step;
-        const end = start + MILLISECONDS_IN_HOUR;
-
-        this.taskValue = new AddTodoListPeriodTaskRequestDto(
-          this.todoListId,
-          this.task.title,
-          this.task.description,
-          start,
-          end);
-      }
-
-      this.fullDayValue = value;
-    }
-  }
-
   public get task(): AddTodoListDayTaskRequestDto | AddTodoListPeriodTaskRequestDto {
-    return this.taskValue ?? (this.taskValue = this.fullDay ? new AddTodoListDayTaskRequestDto(this.todoListId) : new AddTodoListPeriodTaskRequestDto(this.todoListId));
+    return this.taskValue ?? new AddTodoListDayTaskRequestDto();
   }
 
   public add(): Observable<void> {
