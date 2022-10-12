6bce0f0af7c3e3e2526aec2b2351ee028661328e
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-10-12 12:04:38 +0300

    add test for todo list task interceptor

--- src/app/todo-list-task/interceptors/todo-list-task.interceptor.spec.ts
+++ src/app/todo-list-task/interceptors/todo-list-task.interceptor.spec.ts
@@ -3,12 +3,29 @@ import { HttpHandler, HttpRequest, } from '@angular/common/http';
 import { TodoListTaskInterceptor, } from './todo-list-task.interceptor';
 
 describe('TodoListTaskInterceptor', () => {
-  it('intercept', () => {
-    const req : jasmine.SpyObj<HttpRequest<any>> = jasmine.createSpyObj('HttpRequest', [], ['body']);
+  it('intercept should update request body', () => {
+    const req : jasmine.SpyObj<HttpRequest<any>> = jasmine.createSpyObj('HttpRequest', ['clone'], ['body']);
     const next: jasmine.SpyObj<HttpHandler>      = jasmine.createSpyObj('HttpHandler', ['handle']);
 
     const interceptor = new TodoListTaskInterceptor();
 
+    const reqDescs = Object.getOwnPropertyDescriptors(req);
+    const bodyPropSpy = reqDescs.body.get as jasmine.Spy<() => any>;
+
+    bodyPropSpy.and.returnValue({});
+
     interceptor.intercept(req, next);
+
+    expect(req.clone.calls.count())
+      .withContext('req.clone should not be called')
+      .toBe(0);
+
+    expect(next.handle.calls.count())
+      .withContext('next.handle should be called once')
+      .toBe(1);
+
+    expect(next.handle.calls.first().args[0])
+      .withContext('next.handle should be called with original req')
+      .toEqual(req);
   });
 });
