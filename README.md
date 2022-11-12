649a421d7b2d0e15e328281356d87ce309465ca1
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2022-11-12 11:57:32 +0300

    fix pipe tests

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ There are steps to run the app below
 2. Configure and run the API as it described in [readme.md](https://github.com/dennisshevtsov/asp-net-rest-api-sample).
 3. Update variable [apiServer](https://github.com/dennisshevtsov/angular-mvvm-sample/blob/main/src/environments/environment.ts) with the URL of the API.
 4. Run the app with command `ng serve -o`.
+
+There are steps to run the tests.
+1. Karma is configure to run the tests in Firefox. If you want to run the tests in another browser, update the launcher with a proper one in the [karma.conf.js](https://github.com/dennisshevtsov/angular-mvvm-sample/blob/main/karma.conf.js).
+2. Run the tests with commang `ng test`.
--- src/app/todo-list-task/pipes/todo-list-task-time.pipe.spec.ts
+++ src/app/todo-list-task/pipes/todo-list-task-time.pipe.spec.ts
@@ -1,84 +1,97 @@
-// import { inject, TestBed,      } from '@angular/core/testing';
+import { inject, TestBed,      } from '@angular/core/testing';
 
-// import { Formatter,            } from 'src/app/core';
-// import { TodoListTaskTimePipe, } from './todo-list-task-time.pipe';
+import { Formatter,                           } from 'src/app/core';
+import { SearchTodoListDayTaskResponseDto,
+         SearchTodoListPeriodTaskResponseDto, } from 'src/app/todo-list-task/api';
+import { TodoListTaskTimePipe,                } from './todo-list-task-time.pipe';
 
-// describe('TodoListTaskTimePipe', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [
-//         TodoListTaskTimePipe,
-//         {
-//           provide: Formatter,
-//           useValue: jasmine.createSpyObj(Formatter, ['toLocalDate', 'toLocalDateTime']),
-//         },
-//       ],
-//     });
-//   });
+describe('TodoListTaskTimePipe', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        TodoListTaskTimePipe,
+        {
+          provide: Formatter,
+          useValue: jasmine.createSpyObj(Formatter, ['toLocalDate', 'toLocalDateTime']),
+        },
+      ],
+    });
+  });
 
-//   it('transform should return day', inject(
-//     [
-//       Formatter,
-//       TodoListTaskTimePipe,
-//     ],
-//     (
-//       formatterSpy: jasmine.SpyObj<Formatter>,
-//       pipe        : TodoListTaskTimePipe,
-//     ) => {
-//       const date = 'day';
+  it('transform should return day', inject(
+    [
+      Formatter,
+      TodoListTaskTimePipe,
+    ],
+    (
+      formatterSpy: jasmine.SpyObj<Formatter>,
+      pipe        : TodoListTaskTimePipe,
+    ) => {
+      const date = 'day';
 
-//       formatterSpy.toLocalDate.and.returnValue(date);
+      formatterSpy.toLocalDate.and.returnValue(date);
 
-//       const unformatted = new TodoListTaskDateDto(1000, true);
-//       const formatted = pipe.transform(unformatted);
+      const unformatted = new SearchTodoListDayTaskResponseDto(
+        'test todo list task id',
+        false,
+        'test todo list task title',
+        'test todo list task description',
+        1000);
+      const formatted = pipe.transform(unformatted);
 
-//       expect(formatted)
-//         .withContext('transform should return date')
-//         .toBe(date);
+      expect(formatted)
+        .withContext('transform should return date')
+        .toBe(date);
 
-//       expect(formatterSpy.toLocalDate.calls.count())
-//         .withContext('toLocalDate should be called')
-//         .toBe(1);
+      expect(formatterSpy.toLocalDate.calls.count())
+        .withContext('toLocalDate should be called')
+        .toBe(1);
 
-//       expect(formatterSpy.toLocalDate.calls.first().args[0])
-//         .withContext('toLocalDate should be called with correct params')
-//         .toBe(unformatted.day);
-//   }));
+      expect(formatterSpy.toLocalDate.calls.first().args[0])
+        .withContext('toLocalDate should be called with correct params')
+        .toBe(unformatted.date);
+  }));
 
-//   it('transform should return full formatted date', inject(
-//     [
-//       Formatter,
-//       TodoListTaskTimePipe,
-//     ],
-//     (
-//       formatterSpy: jasmine.SpyObj<Formatter>,
-//       pipe        : TodoListTaskTimePipe,
-//     ) => {
-//       const time = 'datetime';
+  it('transform should return full formatted date', inject(
+    [
+      Formatter,
+      TodoListTaskTimePipe,
+    ],
+    (
+      formatterSpy: jasmine.SpyObj<Formatter>,
+      pipe        : TodoListTaskTimePipe,
+    ) => {
+      const time = 'datetime';
 
-//       formatterSpy.toLocalDateTime.and.returnValue(time);
+      formatterSpy.toLocalDateTime.and.returnValue(time);
 
-//       const unformatted = new TodoListTaskDateDto(1000, false, 2000, 3000);
-//       const formatted = pipe.transform(unformatted);
+      const unformatted = new SearchTodoListPeriodTaskResponseDto(
+        'test todo list task id',
+        false,
+        'test todo list task title',
+        'test todo list task description',
+        2000,
+        3000);
+      const formatted = pipe.transform(unformatted);
 
-//       expect(formatted)
-//         .withContext('transform should return date')
-//         .toBe('datetime - datetime');
+      expect(formatted)
+        .withContext('transform should return date')
+        .toBe('datetime - datetime');
 
-//       expect(formatterSpy.toLocalDate.calls.count())
-//         .withContext('toLocalDate should not be called')
-//         .toBe(0);
+      expect(formatterSpy.toLocalDate.calls.count())
+        .withContext('toLocalDate should not be called')
+        .toBe(0);
 
-//       expect(formatterSpy.toLocalDateTime.calls.count())
-//         .withContext('toLocalDateTime should be called')
-//         .toBe(2);
+      expect(formatterSpy.toLocalDateTime.calls.count())
+        .withContext('toLocalDateTime should be called')
+        .toBe(2);
 
-//       expect(formatterSpy.toLocalDateTime.calls.first().args[0])
-//         .withContext('toLocalDateTime should be called with correct params')
-//         .toBe(unformatted.start);
+      expect(formatterSpy.toLocalDateTime.calls.first().args[0])
+        .withContext('toLocalDateTime should be called with correct params')
+        .toBe(unformatted.begin);
 
-//       expect(formatterSpy.toLocalDateTime.calls.mostRecent().args[0])
-//         .withContext('toLocalDateTime should be called with correct params')
-//         .toBe(unformatted.end);
-//   }));
-// });
+      expect(formatterSpy.toLocalDateTime.calls.mostRecent().args[0])
+        .withContext('toLocalDateTime should be called with correct params')
+        .toBe(unformatted.end);
+  }));
+});
